2faa9b310ed416687eccbdc5c6dbf4c9be38b43c
lijie8 <lijie8@staff.sina.com.cn>
2015-01-26 16:28:33 +0800

    新增添加关联的功能

--- models/sequence.js
+++ models/sequence.js
@@ -2,7 +2,7 @@
  * Created by lijie8 on 2015/1/25.
  */
 var mongoose = require('mongoose')
-var SequenceSchema = require('../schemas/task')
+var SequenceSchema = require('../schemas/sequence')
 var Sequence = mongoose.model('sequence', SequenceSchema)
 
 module.exports = Sequence
--- public/js/relation.js
+++ public/js/relation.js
@@ -1,21 +1,25 @@
 /**
  * Created by lijie8 on 2015/1/23.
  */
-function display(id, version_name) {
-    var target = $('#'+id)
+function display(display_id,input_id, version_name) {
+    var target = $('#'+display_id)
     target.html(version_name)
+    target = $('#'+input_id)
+    target.val(version_name)
 }
 
 function displayModel(model_name) {
     var target = $('#model_display')
     target.html(model_name)
+    target = $('#model_input')
+    target.val(model_name)
     var tag_menu = $('#tag_menu')
     tag_menu.html("")
     for (var i =0; i<models.length;i++) {
         if (models[i].model_name == model_name) {
 
             for (var j = 0; j < models[i].tags.length; j++) {
-                tag_menu.append("<li><a onclick=\"display(\'tag_display\',\'" + models[i].tags[j].tag_name + "\')\" class=\"btn\">" + models[i].tags[j].tag_name + "</a></li>")
+                tag_menu.append("<li><a onclick=\"display(\'tag_display\',\'tag_input\',\'" + models[i].tags[j].tag_name + "\')\" class=\"btn\">" + models[i].tags[j].tag_name + "</a></li>")
             }
             break;
         }
